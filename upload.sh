a342305c4761b5daf818a27f67ac067b9d426238
PacoReinaCampo <frareicam@gmail.com>
2021-02-09 01:52:00 +0100

    Working in PU-RISCV

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "new commit"
+git commit -m "Working in PU-RISCV"
 git push origin master
