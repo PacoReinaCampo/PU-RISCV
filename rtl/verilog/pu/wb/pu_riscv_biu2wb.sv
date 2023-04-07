af032c0c2bf5ef7bcace136ceaa6e01b3efd6ee2
PacoReinaCampo <frareicam@gmail.com>
2023-04-08 01:12:34 +0200

    Working in PU-RISCV 🔨

--- rtl/verilog/pu/wb/pu_riscv_biu2wb.sv
+++ rtl/verilog/pu/wb/pu_riscv_biu2wb.sv
@@ -37,7 +37,7 @@
  *
  * =============================================================================
  * Author(s):
- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+ *   Francisco Javier Reina Campo <pacoreinacampo@queenfield.tech>
  */
 
 import peripheral_wb_pkg::*;
